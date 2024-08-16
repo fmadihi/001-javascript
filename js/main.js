ed00e16752e4bc69977f2b0fbc18d54fe11fe40d
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-08-16 16:28:24 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,10 +18,32 @@ let num01 = 100;
     console.log(num01 + num02);
 }
 console.log("num01 is: " + num01);
-console.log("num02 is: " + num02);
+//console.log("num02 is: " + num02);
 
 var firstName = "Fatemeh"
 var lastName = "Madihi"
 console.log("My first name is: " + firstName + " and My last name is: " + lastName);
 
 console.log(`My first name is: ${firstName} and My last name is: ${lastName} `);
+
+let MyFirstname = prompt("Enter your firstname","empty");
+let Mylastname = prompt("Enter your lastname","empty");
+console.log(`Your first name is ${MyFirstname} and your last name is ${Mylastname}`);
+
+
+var x = 10;
+var y = 11;
+var result = ++x+y++;
+console.log(`result is: ${result}`);
+console.log(`y is: ${y}`);
+var x = 10;
+var y = "10";
+let result0 = x===y;
+let res = x == y;
+console.log(result0);
+console.log(res);
+
+var x = 10;
+var y = 20;
+var resault02 = x>y ? "Yes X more than Y" : "No Y more than X";
+console.log(resault02);
