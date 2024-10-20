80613e034a943fd3428b508652cf6a1681b31bcf
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-10-21 00:43:37 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -304,3 +304,9 @@ let demo = document.getElementById("demo");
 data = data.concat(new_data);
 demo.innerText = data.toString();
 console.log(data);
+let data = ["Fatemeh", "Madihi", 30, true];
+let new_data = [1, 2, 3, 4]
+let demo = document.getElementById("demo");
+data.push(new_data);
+demo.innerText = data.toString();
+console.log(data);
