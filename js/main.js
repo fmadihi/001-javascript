18e16d21bc9df7a86a944b8f756541f57a60b1f0
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-12-18 00:11:57 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+
+let data = document.getElementById("data");
+let search = document.getElementById("search");
+let demo = document.getElementById("demo");
+let demoText = demo.innerText;
+search.addEventListener("click", function () {
+  let word = data.value.trim();
+  let wordNew = new RegExp(word, "g");
+  demo.innerHTML = demoText.replace(wordNew, `<mark>${word}</mark>`);
+});
 let data = document.getElementById("data");
 let search = document.getElementById("search");
 let demo = document.getElementById("demo");
